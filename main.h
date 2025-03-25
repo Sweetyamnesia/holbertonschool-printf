6547381ca0c091dead96d129233ab37cedb3ef26
Sweetyamnesia <angela.r@live.fr>
2025-03-25 15:48:26 +0100

    ajout de la structure suite à la suppression à cause d'un merge

--- main.h
+++ main.h
@@ -6,6 +6,16 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/**
+ * struct format_spec - Structure qui définit les différents formats.
+ * @specifier: le caractère représentant le format (%c, %s, %d, etc.).
+ * @function: pointeur vers la fonction qui gère ce format.
+ */
+typedef struct format_spec
+{
+	char specifier;
+	int (*function)(va_list);
+} format_spec_t;
 
 int _printf(const char *format, ...);
 int print_char(va_list args);
