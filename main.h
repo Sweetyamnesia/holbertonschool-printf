bd1bce1abfe0d69b24aaa210737b2a2c277287bd
Sweetyamnesia <angela.r@live.fr>
2025-03-25 14:07:28 +0100

    ajouts de la strcuture et des différents types de formats avec les fonctions

--- main.h
+++ main.h
@@ -5,6 +5,28 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+
+/**
+ * struct format_spec - structure qui définit les différents formats.
+ *
+ * @specifier: la spécificité.
+ * @function: pointeur de la fonction.
+ */
+
+typedef struct format_spec
+{
+	char specifier;
+	int (*function)(va_list);
+} format_spec_t;
+
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_int(va_list args);
+int print_number(int n);
+
+format_spec_t *get_formats(void);
+
+
 
 #endif
