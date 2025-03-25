676feb4f86ac93270a28672f8bbe1895f8e3e331
Sweetyamnesia <angela.r@live.fr>
2025-03-25 15:08:41 +0100

    mise à jour du fichier main.h

---
+++ main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct format_spec - structure qui définit les différents formats.
+ *
+ * @specifier: la spécificité.
+ * @function: pointeur de la fonction.
+ */
+
+typedef struct format_spec
+{
+	char specifier;
+	int (*function)(va_list);
+} format_spec_t;
+
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_int(va_list args);
+int print_number(int n);
+
+format_spec_t *get_formats(void);
+
+
+
+#endif
