ee64da4e5e0d0673ad884acc9f928c288e8446c5
Victoire07 <victoireboutin07@gmail.com>
2025-03-25 23:04:35 +0100

    Fonction afficher un caractère seul et fonction affichant une chaine de caractère %c et %s

---
+++ fonctions.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_char - Affiche un caractere
+ * @display: Liste des arguments
+ *
+ * Return: Nombre de caracteres imprimes (1)
+ */
+
+int print_char(va_list display)
+{
+	int i;
+
+	i = va_arg(display, int);
+
+	_putchar(i);
+
+	return(1);
+}
+
+/**
+ * print_string - Affiche une chaine de caracteres
+ * @display: Liste des arguments
+ *
+ * Return: Nombre de caracteres imprimes
+ */
+
+int print_string(va_list display)
+{
+	char *string;
+	int count;
+
+	string = va_arg(display, char *);
+
+	if (string == NULL)
+	{
+		string = "(null)";
+	}
+
+	for (count = 0; string[count] != '\0'; count++)
+	{
+		_putchar(string[count]);
+	}
+	return (count);
+}
