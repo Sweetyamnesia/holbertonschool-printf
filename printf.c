9b06299773eb17ec927822ab631b6523a800c95c
Victoire07 <victoireboutin07@gmail.com>
2025-03-26 18:13:00 +0100

    Ajout de l'ancienne fonction printf contenant moins d'erreurs que la version n-1

--- printf.c
+++ printf.c
@@ -10,14 +10,11 @@ int _printf(const char *format, ...)
 {
 	va_list list;
 	int i = 0, j = 0, count = 0, similar = 0;
-	
+
 	format_spec_t spec_array[] = {
-		{'c', print_char}, {'s', print_string}, {'d', print_int},
-		{'i', print_int}, {'\0', NULL}
+	{'c', print_char}, {'s', print_string}, {'d', print_int},
+	{'i', print_int}, {'\0', NULL}
 	};
-	
-	if (format == NULL)
-	return (-1);
 
 	va_start(list, format);
 	while (format && format[i])
@@ -26,31 +23,16 @@ int _printf(const char *format, ...)
 		{
 			i++;
 
-			if (format[i] == '\0')
-				return (-1);
-
-				if (format[i] == '%')
-				{
-					putchar('%');
-					count++;
-				}
-
 			for (j = 0; spec_array[j].specifier != '\0'; j++)
 			{
 				if (format[i] == spec_array[j].specifier)
 				{
 					count += spec_array[j].function(list);
-					similar = 1;
 					break;
 				}
 			}
-			if (!similar)
-			{
-			_putchar('%');
-			_putchar(format[i]);
-			count += 2;
-			}
 		}
+		
 		else
 		{
 			_putchar(format[i]);
