1935310c4311e1e9279b2bc74027f0a7773b240f
Victoire07 <victoireboutin07@gmail.com>
2025-03-26 16:06:18 +0100

    Ajout du fichier 'main.h de ma branche vivi a la branche main

--- main.h
+++ main.h
@@ -23,5 +23,8 @@ int _putchar(char c);
 int print_char(va_list list);
 int print_string(va_list list);
 int print_int(va_list list);
+int print_percent(va_list list);
+int print_recursion(int n);
+int print_int(va_list list);
 
 #endif
