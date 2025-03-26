f6eba9eb724912c14f3e93cc1279586638c058eb
Victoire07 <victoireboutin07@gmail.com>
2025-03-26 18:11:09 +0100

    Ajout condition si il n y a pas de similarité entre les formats et le tableau des spécificateurs

--- printf.c
+++ printf.c
@@ -10,11 +10,14 @@ int _printf(const char *format, ...)
 {
 	va_list list;
 	int i = 0, j = 0, count = 0, similar = 0;
-
+	
 	format_spec_t spec_array[] = {
-	{'c', print_char}, {'s', print_string}, {'d', print_int},
-	{'i', print_int}, {'\0', NULL}
+		{'c', print_char}, {'s', print_string}, {'d', print_int},
+		{'i', print_int}, {'\0', NULL}
 	};
+	
+	if (format == NULL)
+	return (-1);
 
 	va_start(list, format);
 	while (format && format[i])
@@ -23,21 +26,30 @@ int _printf(const char *format, ...)
 		{
 			i++;
 
+			if (format[i] == '\0')
+				return (-1);
+
+				if (format[i] == '%')
+				{
+					putchar('%');
+					count++;
+				}
+
 			for (j = 0; spec_array[j].specifier != '\0'; j++)
 			{
 				if (format[i] == spec_array[j].specifier)
 				{
 					count += spec_array[j].function(list);
+					similar = 1;
 					break;
 				}
 			}
-		}
-
-		if (!similar)
-		{
+			if (!similar)
+			{
 			_putchar('%');
 			_putchar(format[i]);
 			count += 2;
+			}
 		}
 		else
 		{
