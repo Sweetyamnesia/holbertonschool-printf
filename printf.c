a0fa720643275f8ed1c5242f28781f110f3b322d
Victoire07 <victoireboutin07@gmail.com>
2025-03-27 16:51:04 +0100

    Retout à l'ancienne version du code car non aboutissement des modifs

--- printf.c
+++ printf.c
@@ -6,10 +6,10 @@
  * Return:  le nombre de caracteres imprimes
  */
 
-int _printf(const char *format, ...)
+ int _printf(const char *format, ...)
 {
     va_list list;
-    int i = 0, j = 0, count = 0, trouve_specificateur = 0;
+    int i = 0, j = 0, count = 0;
 
     format_spec_t spec_array[] = {
     {'c', print_char}, {'s', print_string}, {'d', print_int},
@@ -19,35 +19,26 @@ int _printf(const char *format, ...)
     if (format == NULL)
         return (-1);
 
-    if (*(format + 1) == '\0')
+    if (*(format + 1) == 0)
     {
         return (-1);
     }
     
 
     va_start(list, format);
-    while (format[i])
+    while (format && format[i])
     {
 
         if (format[i] == '%')
         {
             i++;
-            trouve_specificateur = 0;
 
             for (j = 0; spec_array[j].specifier != '\0'; j++)
             {
                 if (format[i] == spec_array[j].specifier)
                 {
                     count += spec_array[j].function(list);
-                    trouve_specificateur = 1;
                     break;
-                }
-
-                if (trouve_specificateur == 0)
-                {
-                    _putchar('%');
-                    _putchar(format[i]);
-                    count += 2;
                 }
             }
         }
@@ -62,4 +53,4 @@ int _printf(const char *format, ...)
     va_end(list);
     return (count);
 
-}
+} 
