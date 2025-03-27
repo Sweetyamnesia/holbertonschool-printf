ceb9c05ac110b9ba9e086ec992b994e1e03d7bdd
Victoire07 <victoireboutin07@gmail.com>
2025-03-27 16:48:20 +0100

    Ajout d'une variable vérifiant l'existence d'1 specific connu

--- printf.c
+++ printf.c
@@ -9,7 +9,7 @@
 int _printf(const char *format, ...)
 {
     va_list list;
-    int i = 0, j = 0, count = 0;
+    int i = 0, j = 0, count = 0, trouve_specificateur = 0;
 
     format_spec_t spec_array[] = {
     {'c', print_char}, {'s', print_string}, {'d', print_int},
@@ -19,29 +19,31 @@ int _printf(const char *format, ...)
     if (format == NULL)
         return (-1);
 
-    if (*(format + 1) == 0)
+    if (*(format + 1) == '\0')
     {
         return (-1);
     }
     
 
     va_start(list, format);
-    while (format && format[i])
+    while (format[i])
     {
 
         if (format[i] == '%')
         {
             i++;
+            trouve_specificateur = 0;
 
             for (j = 0; spec_array[j].specifier != '\0'; j++)
             {
                 if (format[i] == spec_array[j].specifier)
                 {
                     count += spec_array[j].function(list);
+                    trouve_specificateur = 1;
                     break;
                 }
 
-                if (spec_array[j].specifier == '\0')
+                if (trouve_specificateur == 0)
                 {
                     _putchar('%');
                     _putchar(format[i]);
