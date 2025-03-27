e36b16e7533e5615a23e7ed556a4b64c2b0cf0bf
Victoire07 <victoireboutin07@gmail.com>
2025-03-27 16:15:07 +0100

    Ajout de la condition pour le caractère suivant = 0

--- printf.c
+++ printf.c
@@ -17,8 +17,13 @@ int _printf(const char *format, ...)
     };
 
     if (format == NULL)
+        return (-1);
 
-    return(-1);
+    if (*(format + 1) == 0)
+    {
+        return (-1);
+    }
+    
 
     va_start(list, format);
     while (format && format[i])
